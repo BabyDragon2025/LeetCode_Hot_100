7fca4217adffa64a4649c6f266cbd8b7a1f50b8c
dongge-c-language <2296954740@qq.com>
2026-03-13 17:19:33 +0800

    Initial commit

---
+++ LeetCode_Hot_100/Test.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+//两数之和
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < nums.size(); i++)
+        {
+            for (j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return { i,j };
+                }
+            }
+        }
+        return { i,j };
+    }
+};
+*/
+
+int main()
+{
+    return 0;
+}
