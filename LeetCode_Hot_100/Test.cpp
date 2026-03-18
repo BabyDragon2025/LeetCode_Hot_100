930bd3d84b0f5c664c10dd1a9973e1797b9db47a
dongge-c-language <2296954740@qq.com>
2026-03-18 18:07:10 +0800

    盛最多水的容器

--- LeetCode_Hot_100/Test.cpp
+++ LeetCode_Hot_100/Test.cpp
@@ -94,6 +94,30 @@ public:
         }
     }
 };*/
+
+//盛最多水的容器
+/*class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0;
+        int r=height.size()-1;
+        int ans=0;
+        while(l<r)
+        {
+            int area=min(height[l],height[r])*(r-l);
+            ans=max(area,ans);
+            if(height[l]<=height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return ans;
+    }
+};*/
 int main()
 {
 
