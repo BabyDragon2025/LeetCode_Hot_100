e92a41b76b701cd21bcd8c24b43081a9a85b46b6
dongge-c-language <2296954740@qq.com>
2026-03-18 18:45:43 +0800

    三数之和

--- LeetCode_Hot_100/Test.cpp
+++ LeetCode_Hot_100/Test.cpp
@@ -118,6 +118,52 @@ public:
         return ans;
     }
 };*/
+
+//三数之和
+/*class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
+        {
+            if(i>=1 &&nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            int l=i+1;
+            int r=n-1;
+            int target=-nums[i];
+            while(l<r)
+            {
+                if(nums[l]+nums[r]==target)
+                {
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(l<r&&nums[l]==nums[l-1])
+                    {
+                        l++;
+                    }
+                    while(l<r&&nums[r]==nums[r+1])
+                    {
+                        r--;
+                    }
+                }
+                else if(nums[l]+nums[r]<target)
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+        }
+        return ans;
+    }
+};*/
 int main()
 {
 
