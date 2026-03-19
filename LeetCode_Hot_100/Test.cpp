c3cf68c645caf0bc937e090542e9f0578354cb68
dongge-c-language <2296954740@qq.com>
2026-03-19 18:01:07 +0800

    接雨水

--- LeetCode_Hot_100/Test.cpp
+++ LeetCode_Hot_100/Test.cpp
@@ -164,6 +164,39 @@ public:
         return ans;
     }
 };*/
+
+//Ω””ÍÀÆ
+/*class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        if(n==0)
+        {
+            return 0;
+        }
+        vector<int> LeftMax(n);
+        LeftMax[0]=height[0];;
+        for(int i=1;i<n;i++)
+        {
+            LeftMax[i]=max(LeftMax[i-1],height[i]);
+        }
+
+        vector<int> RightMax(n);
+        RightMax[n-1]=height[n-1];;
+        for(int i=n-2;i>=0;i--)
+        {
+            RightMax[i]=max(RightMax[i+1],height[i]);
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=min(LeftMax[i],RightMax[i])-height[i];
+        }
+        return ans;
+    }
+};*/
+
 int main()
 {
 
