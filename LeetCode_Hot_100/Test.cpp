9858783361a5c110aeead4163e032bdd3ed39029
Baby_Dragon_2025 <2296954740@qq.com>
2026-03-28 21:52:02 +0800

    滑动窗口最大值

--- LeetCode_Hot_100/Test.cpp
+++ LeetCode_Hot_100/Test.cpp
@@ -283,6 +283,35 @@ public:
     }
 };*/
 
+
+//滑动窗口最大值
+/*
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        deque<int> q;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            while(!q.empty()&&nums[i]>=nums[q.back()])
+            {
+                q.pop_back();
+            }
+            q.push_back(i);
+            while(!q.empty()&&q.front()<=i-k)
+            {
+                q.pop_front();
+            }
+            if(i>=k-1)
+            {
+                res.push_back(nums[q.front()]);
+            }
+        }
+        return res;
+    }
+};*/
+
 int main()
 {
 
