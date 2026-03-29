d6e915ea9a318abff919006f4038586120f91c83
Baby_Dragon_2025 <2296954740@qq.com>
2026-03-29 16:53:19 +0800

    最大子数组和

--- LeetCode_Hot_100/Test.cpp
+++ LeetCode_Hot_100/Test.cpp
@@ -312,6 +312,29 @@ public:
     }
 };*/
 
+
+
+//最大子数组和
+/*
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1]>0)
+            {
+                nums[i]+=nums[i-1];
+            }
+            if(nums[i]>res)
+            {
+                res=nums[i];
+            }
+        }
+        return res;
+    }
+};*/
+
 int main()
 {
 
