5676d3219531ff3a2644927b5fc298712d25ad5d
Baby_Dragon_2025 <2296954740@qq.com>
2026-04-01 19:02:11 +0800

    轮转数组

--- LeetCode_Hot_100/Test.cpp
+++ LeetCode_Hot_100/Test.cpp
@@ -360,6 +360,31 @@ public:
     }
 };*/
 
+
+
+//ÂÖ×ªÊı×é
+/*
+class Solution {
+public:
+
+    void reverse(vector<int>& v,int start,int end)
+    {
+        while(start<end)
+        {
+            swap(v[start],v[end]);
+            start++;
+            end--;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        k%=nums.size();
+        reverse(nums,0,nums.size()-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.size()-1);
+    }
+};*/
+
 int main()
 {
 
